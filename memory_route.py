1163e44a35c46a9fe567cde7356781aab8bea4a4
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-06-24 07:54:52 +0200

    memory routes(add memory, edit memory, delete memory) and update profile data

--- memory_route.py
+++ memory_route.py
@@ -1,8 +1,10 @@
 from flask import Flask, flash, render_template, redirect, url_for, request
 from flask_login import LoginManager, UserMixin, login_required, login_user, current_user, logout_user
+from werkzeug.security import generate_password_hash, check_password_hash
 from helping_functions import load_data, save_data
-from uuid import uuid4
 from datetime import datetime
+from os import path, remove
+from uuid import uuid4
 
 app = Flask(__name__)
 app.secret_key = 'memories@40&65'
@@ -41,12 +43,16 @@ def load_user(user_id):
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'GET':
+        if current_user.is_authenticated:
+            return redirect(url_for('home'))
+
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         user = users_db.get(username)
-        if user and password == user.password:
-            login_user(user)
+        if user and check_password_hash(user.password, password):
+            login_user(user, remember=True)
             print("Logged in successfully")
             return redirect(url_for('home'))
         print("Invalid username or password")
@@ -54,7 +60,12 @@ def login():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    if request.method == 'POST':
+    if request.method == 'GET':
+        if current_user.is_authenticated:
+            return redirect(url_for('home'))
+
+    elif request.method == 'POST':
+        profile_image = request.files['profile_image']
         username = request.form['username']
         email = request.form['email']
         password = request.form['password']
@@ -69,16 +80,22 @@ def register():
             return redirect(url_for('register'))
 
         user_id = str(uuid4())
-        new_user = User(user_id, username, password)
+        hashed_password = generate_password_hash(password, method='scrypt', salt_length=14)
+        new_user = User(user_id, username, hashed_password)
 
         users_db[username] = new_user
         user_data = {
             "user_id": user_id,
             "username": username,
             "email": email,
-            "password": password,
+            "password": hashed_password,
             "memory": []
         }
+
+        if profile_image:
+            image_path = path.join(app.root_path, 'static/images/profiles', profile_image.filename)
+            profile_image.save(image_path)
+            user_data['profile_image'] = url_for('static', filename=f'images/profiles/{profile_image.filename}')
 
         data = load_data()
         data.append(user_data)
@@ -88,7 +105,8 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html')
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/home', methods=['GET', 'POST'])
 @login_required
 def home():
     data = load_data()
@@ -101,30 +119,170 @@ def home():
     if user_data:
         return render_template('home.html', username=user_data['username'], memories=user_data['memory'])
     else:
-        flash('User data not found.')
-        return redirect(url_for('logout'))
+        print('User data not found.')
+        return redirect(url_for('login'))
 
 @app.route('/add_memory', methods=['POST'])
 @login_required
 def add_memory():
     title = request.form['title']
     description = request.form['description']
-    image = request.form['image']
+    image_memory = request.files['image_memory']
     new_memory = {
+        "memory_id": 0,
         "title": title,
         "description": description,
-        "date": datetime.now().strftime("%a %b %d %H:%M:%S %Y"),
-        "image": image if image else None
+        "date": datetime.now().strftime("%A %B %H:%M:%S.%f %p %d-%m-%Y")
     }
+
+    if image_memory:
+        image_path = path.join(app.root_path, "static/images/images_memory", image_memory.filename)
+        image_memory.save(image_path)
+        new_memory["image_memory"] = url_for('static', filename=f"images/images_memory/{image_memory.filename}")
+    else:
+        new_memory["image_memory"] = ""
 
     data = load_data()
     for user in data:
         if user['username'] == current_user.username:
+            new_memory['memory_id'] = len(user['memory']) + 1
             user['memory'].append(new_memory)
             break
 
     save_data(data)
+    print("Add Memory Success")
     return redirect(url_for('home'))
+
+@app.route('/edit_memory', methods=['POST'])
+@login_required
+def edit_memory():
+    memory_id = int(request.form.get('memory_id'))
+    new_title = request.form.get('new_title')
+    new_description = request.form.get('new_description')
+    image_memory = request.files['image_memory']
+
+
+    data = load_data()
+    for user in data:
+        if user['username'] == current_user.username:
+            for memory in user['memory']:
+                if memory['memory_id'] == memory_id:
+                    memory['title'] = new_title
+                    memory['description'] = new_description
+                    memory['date'] = datetime.now().strftime("%A %B %H:%M:%S.%f %p %d-%m-%Y")
+                    if image_memory:
+                        memory_image_path = path.join(app.root_path, "static/images/images_memory", image_memory.filename)
+                        image_memory.save(memory_image_path)
+                        memory_image_path = url_for('static', filename=f"images/images_memory/{image_memory.filename}")
+
+                        old_memory_image = memory['image_memory'].replace("/", "\\")
+                        old_memory_image_filename = (
+                            path.basename(old_memory_image)
+                        ).replace("%20", " ")
+                        old_memory_image_path = path.join(app.root_path, "static/images/profiles", old_memory_image_filename)
+                        if path.exists(old_memory_image_path):
+                            remove(old_memory_image_path)
+                        memory['image_memory'] = memory_image_path
+                        print('Image updated successfully.')
+                    break
+
+    save_data(data)
+    print("Edit Success")
+    return redirect(url_for('home'))
+
+@app.route('/delete_memory', methods=['POST'])
+@login_required
+def delete_memory():
+    memory_id = int(request.form.get('memory_id'))
+
+    data = load_data()
+    for user in data:
+        if user['username'] == current_user.username:
+            for memory in user['memory']:
+                if memory['memory_id'] == memory_id:
+                    user['memory'].remove(memory)
+                    break
+
+    save_data(data)
+    print("Delete Success")
+    return redirect(url_for('home'))
+
+@app.route('/profile', methods=['GET', 'POST'])
+@login_required
+def profile():
+    data = load_data()
+    for user_data in data:
+        if user_data['username'] == current_user.username:
+            user = user_data
+            break
+    return render_template('profile.html', user=user)
+
+@app.route('/update_profile_image', methods=['GET', 'POST'])
+@login_required
+def update_profile_image():
+    old_profile_image = None
+    new_username = request.form.get('new_username')
+    new_email = request.form.get('new_email')
+    current_password = request.form.get('current_password')
+    new_password = request.form.get('new_password')
+    new_confirm_password = request.form.get('new_confirm_password')
+    profile_image = request.files["profile_image"]
+
+    try:
+        data = load_data()
+        for user in data:
+            if user['username'] == current_user.username:
+                user_data = user
+                break
+
+        if profile_image:
+            profile_image_path = path.join(app.root_path, "static/images/profiles", profile_image.filename)
+            profile_image.save(profile_image_path)
+            profile_image_path = url_for('static', filename=f'images/profiles/{profile_image.filename}')
+
+            old_profile_image = user_data['profile_image'].replace("/", "\\")
+            old_profile_image_filename = (
+                path.basename(old_profile_image)
+            ).replace("%20", " ")
+            old_profile_image_path = path.join(app.root_path, "static/images/profiles", old_profile_image_filename)
+            if path.exists(old_profile_image_path):
+                remove(old_profile_image_path)
+            user_data['profile_image'] = profile_image_path
+            print('Image updated successfully.')
+
+        if new_username:
+            user_data['username'] = new_username
+            print('Username updated successfully.')
+        
+        if new_email:
+            user_data['email'] = new_email
+            print('Email updated successfully.')
+
+        if current_password and new_password and new_confirm_password:
+            if check_password_hash(user_data['password'], current_password):
+                if new_password == new_confirm_password:
+                    user_data['password'] = generate_password_hash(new_password)
+                    print('Password updated successfully.')
+                else:
+                    print("New password and confirm password do not match", "danger")
+            else:
+                print("Current password is incorrect", "danger")
+
+        for user in data:
+            if user['username'] == current_user.username:
+                user = user_data
+                break
+
+        save_data(data)
+        print('Profile updated successfully.')
+        if current_password and new_password and new_confirm_password\
+            and check_password_hash(user['password'], current_password) is False\
+            and check_password_hash(user['password'], new_password) is True:
+            logout_user()
+            return redirect(url_for('login'))
+    except Exception as e:
+        print(f"Error: {e}")
+    return redirect(url_for('profile'))
 
 @app.route('/logout')
 @login_required
